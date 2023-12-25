e54255cf223c84582da2202b425d52b936e856ab
Kanaan Alahmad <kanaan92@hotmail.com>
2023-12-25 16:38:36 +0400

    K-SEO-v1.0

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -7,28 +7,37 @@ import Video from "./components/Video";
 import RegisterForm from "./components/RegisterForm";
 import { useSelector } from "react-redux";
 import { selectPage, selectProject } from "../../redux/systemSettingsSlice";
+import Head from "../../components/Layout/PageContainer/Head";
 
 const HomePage = () => {
-  const page = useSelector(selectPage) ?? {};
-  const project = useSelector(selectProject) ?? {};
-  return (
-    Object.keys(project).length !== 0 && (
-      <div>
-        <Header />
-        <Info />
-        <Nearby />
-        <div
-          className="mt-6 flex flex-col justify-center items-center"
-          style={{
-            backgroundColor: page?.Colors?.Primary,
-          }}
-        >
-          <Video />
-          <RegisterForm />
-        </div>
-      </div>
-    )
-  );
+	const page = useSelector(selectPage) ?? {};
+	const project = useSelector(selectProject) ?? {};
+	return (
+		Object.keys(project).length !== 0 && (
+			<div>
+				<Head
+					title={project?.ProjectName.en}
+					desc={project?.InfoSubTitle.en}
+					keywords={
+						"About Ava Real Esatate, Best Real Estate Agency, Dubai Real Esate, Dubai Properties"
+					}
+					canonicalLink={`/${page.slug}/${project?.slug}`}
+				/>
+				<Header />
+				<Info />
+				<Nearby />
+				<div
+					className='mt-6 flex flex-col justify-center items-center'
+					style={{
+						backgroundColor: page?.Colors?.Primary,
+					}}
+				>
+					<Video />
+					<RegisterForm />
+				</div>
+			</div>
+		)
+	);
 };
 
 export default HomePage;
