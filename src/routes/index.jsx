99afa5fab152ec1244f921b5a2ab23194d1302ec
moazabdulhalim <moazabdulhalim@gmail.com>
2023-12-29 14:55:58 +0300

    fix redirection

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,25 +1,36 @@
 import React from "react";
 import { publicRoutes } from "./routesLists";
 import PageLayout from "../components/Layout/PageContainer";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { LandingPages } from "../data/PagesData";
 const withRoute = (routes) => {
-  return routes.map((route) =>
-    route.path == "*" ? (
-      <Route
-        path={route.path}
-        element={
-          <route.element
-            to={`/${LandingPages[0].slug}/${LandingPages[0].projects[0].slug}`}
-            replace
-          />
-        }
-        key={route.path}
-      />
-    ) : (
-      <Route path={route.path} element={<route.element />} key={route.path} />
-    )
-  );
+  const location = useLocation();
+  return routes.map((route) => {
+    if (route.path == "*") {
+      let firstPath = location.pathname.substring(1);
+      let page = LandingPages.find((x) => x.slug == firstPath);
+      return (
+        <Route
+          path={route.path}
+          element={
+            <route.element
+              to={
+                page
+                  ? `/${page.slug}/${page.projects[0].slug}`
+                  : `/${LandingPages[0].slug}/${LandingPages[0].projects[0].slug}`
+              }
+              replace
+            />
+          }
+          key={route.path}
+        />
+      );
+    } else {
+      return (
+        <Route path={route.path} element={<route.element />} key={route.path} />
+      );
+    }
+  });
 };
 
 function Router() {
--- src/routes/routesLists.jsx
+++ src/routes/routesLists.jsx
@@ -1,7 +1,7 @@
 import { lazy } from "react";
 import { Navigate } from "react-router-dom";
 import { LandingPages } from "../data/PagesData.js";
-const home = lazy(() => import("../pages/home/index.jsx"));
+import HomePage from "../pages/home/index.jsx";
 const thankYou = lazy(() => import("../pages/thankYou/index.jsx"));
 
 let projectsRoutes = [];
@@ -9,11 +9,10 @@ LandingPages.map((item) => {
   return item.projects.map((ps) => {
     projectsRoutes.push({
       path: `/${item.slug}/${ps.slug}`,
-      element: home,
+      element: HomePage,
     });
   });
 });
-// console.log(projectsRoutes.map((i) => i.path));
 export const publicRoutes = [
   ...projectsRoutes,
   {
